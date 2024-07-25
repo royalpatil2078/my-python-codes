783b3b3ed502b59d76c9f822ab6affb5c938de8b
royalpatil2078 <patildigvijay2020@gmail.com>
2024-07-25 19:37:19 +0530

    Updated some changes in palindrome code

--- my-basic-codes/palindrome.py
+++ my-basic-codes/palindrome.py
@@ -1,6 +1,10 @@
 #palindrome is a word which is same as its reverse 
 def palindrome(s):
     return s==s[::-1]
-print(palindrome("Digvijay"))
-print(palindrome("racecar"))
-#Saurabh Patil
+
+user_input = input("Enter a string to check palindrome: ")
+palindrome_string = palindrome(user_input)
+print(("wether the input string is palindrome:", palindrome_string))
+#print(palindrome("Digvijay"))
+#print(palindrome("racecar"))
+
