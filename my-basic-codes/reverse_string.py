e242dc168259d57403e957465a27818a0eb2d72f
royalpatil2078 <patildigvijay2020@gmail.com>
2024-07-25 19:23:57 +0530

    Reverse string file has been added in repo

---
+++ my-basic-codes/reverse_string.py
@@ -0,0 +1,8 @@
+def rev_string(s):
+    return s[::-1]
+
+user_input = input("Enter a string to reverse: ")
+reversed_string = rev_string(user_input)
+print("Reversed string:", reversed_string)
+
+#print(rev_string("Digvijay"))
